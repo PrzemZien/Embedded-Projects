aaf150263f8a88208711322b6b2252232cee9317
PrzemZien <145202128+PrzemZien@users.noreply.github.com>
2025-07-13 19:00:39 +0200

    Add files via upload

---
+++ STM32-Projects/RTOS/RoundRobinScheduler/Inc/osKernel.h
@@ -0,0 +1,18 @@
+/*
+ * osKernel.h
+ *
+ *  Created on: Jul 10, 2025
+ *      Author: przem
+ */
+
+#ifndef OSKERNEL_H_
+#define OSKERNEL_H_
+#include "stm32f4xx.h"
+#include "stdint.h"
+
+void osKernelInit(void);
+uint8_t osKernelAddThread(void(*task0)(void),void(*task1)(void),void(*task2)(void));
+void osKernelLaunch(uint32_t quanta);
+
+
+#endif /* OSKERNEL_H_ */
